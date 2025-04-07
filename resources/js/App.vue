a30c3516c6276a6514ab4cf7eb2b6c8f98ad13ba
Komiljonov O <uzokdeveloper@gmail.com>
2025-04-07 19:41:06 +0500

    MINOR: Display Contact list

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -193,6 +193,15 @@ export default {
             }
         };
 
+        const getRooms = async () => {
+            try{
+                const response = await axios.get('/rooms');
+                contacts.value = response.data;
+            }catch (err){
+                console.error(err);
+            }
+        };
+
         const sendMessage = async () => {
             if (newMessage.value.trim() === '') return;
 
